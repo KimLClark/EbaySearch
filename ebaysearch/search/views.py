909020f9277fab2d20822c9247df5465e6dabb6e
KimLClark <kim@clarkfamilycentral.net>
2021-01-31 20:38:54 -0800

    End of template intro

--- ebaysearch/search/views.py
+++ ebaysearch/search/views.py
@@ -2,4 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 def index(request):
-    return HttpResponse('Hello World (from search.index)')
+    n = 'Bob'
+    animals = ['Zebra', 'Cat', 'Dog']
+    return render(request, 'search/index.html', context={'name':n, 'animals':animals})
